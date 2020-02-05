b82f7b54250819658e0b041f7b6436f10b60c8ab
Dogucan Yaman <41586251+yamand16@users.noreply.github.com>
2020-02-05 11:22:10 +0100

    Add files via upload

---
+++ install_libraries.sh
@@ -0,0 +1,16 @@
+# Install matplotlib
+conda install -c conda-forge matplotlib
+
+# Install scikit image
+conda install -c anaconda scikit-image
+
+# Install scikit learn
+conda install -c anaconda scikit-learn
+
+# Install scipy
+conda install -c anaconda scipy
+
+# Install numpy
+conda install -c anaconda numpy
+
+
