53e0c21ed56abfb77795f0a4014056a4fc44b93d
Dogucan Yaman <41586251+yamand16@users.noreply.github.com>
2020-02-07 10:47:01 +0100

    Update install_pytorch.sh

--- install_pytorch.sh
+++ install_pytorch.sh
@@ -1,10 +1,11 @@
 # Install PyTorch
-#conda install -c pytorch pytorch
+conda install -c pytorch pytorch
 
 # Install Torch Vision
-#conda install -c pytorch torchvision
+conda install -c pytorch torchvision
 
-# Alternative way to install PyTorch and Torch Vision with Specific Cuda
-conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
+# Alternative way to install PyTorch and Torch Vision with Specific Cuda and Cudnn
+#conda install pytorch torchvision cudatoolkit=10.1 cudnn -c pytorch
+
 # You can use below link to get appropriate PyTorch installation command for your system.
 #https://pytorch.org/
