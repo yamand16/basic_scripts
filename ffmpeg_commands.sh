6ee4ad6bd4e64fb84c912af49d2d1e415f1fc449
Dogucan Yaman <41586251+yamand16@users.noreply.github.com>
2022-09-07 10:37:02 +0200

    Update ffmpeg_commands.sh

--- ffmpeg_commands.sh
+++ ffmpeg_commands.sh
@@ -1,6 +1,9 @@
 # Concatenate two videos side-by-side
 ffmpeg -i video1.mp4 -i video2.mp4 -filter_complex '[0:v]pad=iw*2:ih[int];[int][1:v]overlay=W/2:0[vid]' -map [vid] -c:v libx264 -crf 23 -preset veryfast output.mp4
 
+# Concatenate multiple audio files
+ffmpeg -i TTS_gen1.wav -i TTS_gen2.wav -i TTS_gen3.wav -i TTS_gen4.wav -filter_complex '[0:0][1:0][2:0][3:0]concat=n=4:v=0:a=1[out]' -map '[out]' output.wav
+
 # Crop video by providing starting time and length
 ffmpeg -ss 00:00:00 -t 00:00:00 -i input.mp4 -vcodec copy -acodec copy output.mp4
 
