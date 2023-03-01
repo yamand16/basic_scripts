564eedf040919eb4169bb2b360ebfcd6d515e6b8
Dogucan Yaman <41586251+yamand16@users.noreply.github.com>
2023-03-01 11:50:34 +0100

    Update change_sampling_rate_of_an_audio.sh

--- change_sampling_rate_of_an_audio.sh
+++ change_sampling_rate_of_an_audio.sh
@@ -4,6 +4,9 @@ for i in $(find */audio/* -type f); do
 	ext="${i##*.}"	
 	if [[ $ext == wav ]]
 	then
+		#IFS="/"
+		#read -a strarr <<< "$i"
+		#"new_folder_name/${strarr[1]}"
 		p=${i%".wav"}
 		echo $i
 		echo $p'_16000.wav'
