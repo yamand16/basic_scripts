5a37751ebbcc76594e77b12f4ae576f5af8dea88
Irem Eyiokur <irem.eyiokur@gmail.com>
2024-05-13 17:47:15 +0200

    Added command to draw text on a video

--- ffmpeg_commands.sh
+++ ffmpeg_commands.sh
@@ -1,14 +1,14 @@
 # Concatenate two videos side-by-side
 ffmpeg -i video1.mp4 -i video2.mp4 -filter_complex '[0:v]pad=iw*2:ih[int];[int][1:v]overlay=W/2:0[vid]' -map [vid] -c:v libx264 -crf 23 -preset veryfast output.mp4
 
-#Concatenate two videos side-by-side with text
-#[0:a]-> index 0 means audio from first video is used
+# Concatenate two videos side-by-side with text
+# [0:a]-> index 0 means audio from first video is used
 fmpeg -i video1.mp4 -i video2.mp4 -i -filter_complex "[0:v]drawtext=text='Text 1':x=50:y=680:fontsize=26:fontcolor=green[txt1];[1:v]drawtext=text='Text 2':x=50:y=680:fontsize=26:fontcolor=green[txt2];[txt1][txt2]hstack=inputs=2[v];[0:a]aformat=channel_layouts=stereo[a]" -map "[v]" -map "[a]" -r frame_rate output.mp4
 
-#Concatenate multiple videos side-by-side with text
-#Follow this format for any number of videos
-#[0:a]-> change index which audio you would like to use
-#[0:a]aformat=channel_layouts=stereo[a] -> [0:a][1:a][2:a]amix=inputs=3[a] use this if you want mix audio
+# Concatenate multiple videos side-by-side with text
+# Follow this format for any number of videos
+# [0:a]-> change index which audio you would like to use
+# [0:a]aformat=channel_layouts=stereo[a] -> [0:a][1:a][2:a]amix=inputs=3[a] use this if you want mix audio
 ffmpeg -i video1.mp4 -i video2.mp4 -i video3.mp4 -filter_complex "[0:v]drawtext=text='Text 1':x=50:y=680:fontsize=26:fontcolor=green[txt1];[1:v]drawtext=text='Text 2':x=50:y=680:fontsize=26:fontcolor=green[txt2];[2:v]drawtext=text='Text 3':x=50:y=680:fontsize=26:fontcolor=green[txt3];[txt1][txt2][txt3]hstack=inputs=3[v];[0:a]aformat=channel_layouts=stereo[a]" -map "[v]" -map "[a]" -r frame_rate output.mp4
 
 # Concatenate multiple audio files
@@ -48,6 +48,9 @@ ffmpeg -i input.mp4 -vcodec libx265 -crf 28 output.mp4 # or libx264. Increasing 
 
 # Change FPS of the video
 ffmpeg -i input.mp4 -filter:v fps=30 output.mp4
+
+# Add text to the video
+ffmpeg -i input_video.mp4 -vf "drawtext=text='Your Text Here':x=100:y=100:fontsize=26:fontcolor=white" -c:a copy output_video.mp4
 
 # Combine an audio and a video
 ffmpeg -y -i input1.wav -i input2.mp4 -strict -2 -q:v 1 output.mp4
